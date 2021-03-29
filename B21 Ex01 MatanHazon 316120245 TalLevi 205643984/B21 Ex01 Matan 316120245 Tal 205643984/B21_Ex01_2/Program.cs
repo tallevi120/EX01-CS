f2e09543636f6361f26df0a9231aba01dabd66ce
tallevi120 <tallevi120@gmail.com>
2021-03-29 20:42:58 +0300

    Create Program.cs

---
+++ B21 Ex01 MatanHazon 316120245 TalLevi 205643984/B21 Ex01 Matan 316120245 Tal 205643984/B21_Ex01_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace B21_Ex01_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int height = 5;
+            PrintHourglass(height);
+        }
+        public static void PrintHourglass(int i_HeightOfHourglass, int i_NumOfSpaces = 0)
+        {
+            if(i_HeightOfHourglass > 0)
+            {
+                PrintLineInHourglass(i_HeightOfHourglass, i_NumOfSpaces);
+                if (i_HeightOfHourglass != 1)
+                {
+                    PrintHourglass(i_HeightOfHourglass - 2, i_NumOfSpaces + 1);
+                    PrintLineInHourglass(i_HeightOfHourglass, i_NumOfSpaces);
+                }
+            }
+        }
+        public static void PrintLineInHourglass(int i_HeightOfHourglass, int i_NumOfSpaces = 0)
+        {
+            int sizeOfLine = i_HeightOfHourglass;
+            while (i_NumOfSpaces != 0)
+            {
+                Console.Write(" ");
+                i_NumOfSpaces--;
+            }
+            while (sizeOfLine != 0)
+            {
+                Console.Write("*");
+                sizeOfLine--;
+            }
+            Console.Write("\n");
+        }
+    }
+}

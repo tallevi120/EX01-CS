e094c9d7596e847219aea13c9a5b8e5982f0de73
tallevi120 <tallevi120@gmail.com>
2021-03-29 20:42:29 +0300

    Create Program.cs

---
+++ B21 Ex01 MatanHazon 316120245 TalLevi 205643984/B21 Ex01 Matan 316120245 Tal 205643984/B21_Ex01_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace B21_Ex01_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int     heightForHourglass;
+            Console.WriteLine(string.Format("Hello, Please enter integer number for the height of the hourglass :"));
+            heightForHourglass = TakeNumberFromTheUser();
+            B21_Ex01_2.Program.PrintHourglass(heightForHourglass);
+        }
+        public static int TakeNumberFromTheUser()
+        {
+            int    heightForHourglass = default;
+            string inputFromUser = default;
+            bool   i_IfInputInvalid = false;
+            while(i_IfInputInvalid == false)
+            {
+                inputFromUser = Console.ReadLine();
+                i_IfInputInvalid = int.TryParse(inputFromUser, out heightForHourglass);
+                if(i_IfInputInvalid == false)
+                {
+                    Console.WriteLine("Invalid input, please enter only integer :");
+                }
+            }
+            if (CheckIfDouble(heightForHourglass))
+            {
+                heightForHourglass--;
+            }
+            return heightForHourglass;
+        }
+        public static bool CheckIfDouble(int i_InputNumber)
+        {
+            return (i_InputNumber % 2) == 0;
+        }
+    }
+}
